9cad94c8ee4479d8e38be0a29b743208a4cc908c
gauravkumar9130 <55126211+gauravkumar9130@users.noreply.github.com>
2021-08-27 15:56:49 +0530

    Update gvisor.sh

--- gvisor.sh
+++ gvisor.sh
@@ -1,7 +1,6 @@
 echo "please type username"
 read username
 useradd $username
-
 echo "please type $username passsword"
 read password
 echo $password | passwd --stdin $username
@@ -9,7 +8,6 @@ cat >> /etc/sudoers <<EOF
 $username ALL=(ALL) NOPASSWD: ALL
 EOF
 su - $username 
-
 echo "Install libvirtd,kvm and minikube and  kubectl"
 sudo yum -y install epel-release
 sudo yum -y install libvirt qemu-kvm virt-install virt-top libguestfs-tools bridge-utils
@@ -17,19 +15,15 @@ sudo systemctl start libvirtd
 sudo systemctl enable libvirtd
 systemctl status libvirtd
 sudo usermod -a -G libvirt $(whoami)
-
-
 wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 chmod +x minikube-linux-amd64
 sudo mv minikube-linux-amd64 /usr/local/bin/minikube
 minikube version
-
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x kubectl
 sudo mv kubectl  /usr/local/bin/
 kubectl version --client -o json
-
-Start and enable plugins:
+echo "Start and enable plugins"
 minikube start --container-runtime=containerd  --docker-opt containerd=/var/run/containerd/containerd.sock
 minikube addons list -> to list plugins 
 minikube addons enable gvisor
